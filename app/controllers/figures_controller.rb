ee69a3e9d0adf4a33edccabcd90fe05cb64e1b4f
Audrea Cook <audreacwork@gmail.com>
2019-09-09 09:07:57 -0600

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,29 @@
 class FiguresController < ApplicationController
-  # add controller methods
+  get '/figures' do
+    @figures = Figure.all
+    erb :'figures/index'
+  end
+
+  get '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :'figures/new'
+  end
+
+  post 'figures' do
+    @figure = Figure.create(name: params['figure'])
+    unless @figure[:title][:name].empty?
+      @figure.titles << Title.create(params[:title])
+    end
+    unless @figure[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+    @figure.save
+    redirect '/figures'
+
+  end
+
+
+
+
 end
